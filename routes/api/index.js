970bf7e82c2718b00c8e359b075aa95ba24861c2
Erik Jones <jonzee1914@gmail.com>
2022-02-18 21:52:20 -0500

    still not working

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const commentRoutes = require('./comment-routes');
+const commentRoutes = require('./comment-routes.js');
 const pizzaRoutes = require('./pizza-routes');
 
 router.use('/comments', commentRoutes);
